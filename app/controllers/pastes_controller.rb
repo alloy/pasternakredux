b514ef34651551b82f265bfc8db31d0f2e2013de
Eloy Duran <eloy.de.enige@gmail.com>
2008-06-07 01:05:40 +0200

    Added views for creating a paste.

--- app/controllers/pastes_controller.rb
+++ app/controllers/pastes_controller.rb
@@ -1,6 +1,10 @@
 class PastesController < ApplicationController
   allow_access :all
   
+  def new
+    @paste = Paste.new
+  end
+  
   def create
     return redirect_to(new_paste_url) unless params[:patch_style] == 'monkeypatch'
     
@@ -8,5 +12,9 @@ class PastesController < ApplicationController
     if @paste.save
       redirect_to paste_url(@paste)
     end
+  end
+  
+  def show
+    @paste = Paste.find(params[:id])
   end
 end
--- test/functional/pastes_controller_test.rb
+++ test/functional/pastes_controller_test.rb
@@ -3,6 +3,12 @@ require File.expand_path('../../test_helper', __FILE__)
 describe "PastesController" do
   tests PastesController
   
+  it "should show a new page" do
+    get :new
+    status.should.be :success
+    assigns(:paste).should.be.instance_of Paste
+  end
+  
   it "should create a new paste with the correct token" do
     lambda {
       post :create, :paste => { :code => 'def foo; puts "foo"; end' }, :patch_style => 'monkeypatch'
@@ -18,5 +24,19 @@ describe "PastesController" do
       post :create, :paste => { :code => 'def foo; puts "foo"; end' }, :patch_style => 'bla'
     }.should.not.differ('Paste.count')
     should.redirect_to new_paste_url
+  end
+  
+  it "should NOT create a new paste without a token" do
+    lambda {
+      post :create, :paste => { :code => 'def foo; puts "foo"; end' }, :patch_style => nil
+    }.should.not.differ('Paste.count')
+    should.redirect_to new_paste_url
+  end
+  
+  it "should show a paste" do
+    paste = pastes(:ruby_code)
+    get :show, :id => paste.id
+    status.should.be :success
+    assigns(:paste).should == paste
   end
 end
