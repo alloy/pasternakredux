f669fc21d4781806c3519707f40f43039e0ada46
Eloy Duran <eloy.de.enige@gmail.com>
2008-06-06 22:33:09 +0200

    Added PastesController stub.

---
+++ app/controllers/pastes_controller.rb
@@ -0,0 +1,2 @@
+class PastesController < ApplicationController
+end
---
+++ test/functional/pastes_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class PastesControllerTest < ActionController::TestCase
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
