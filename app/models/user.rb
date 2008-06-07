a03bb8af0e56bcd981cf5d56c44dbdd5ca7d2485
Eloy Duran <eloy.de.enige@gmail.com>
2008-06-07 19:23:14 +0200

    SHA1 digest a password before it's saved to the db. Added User::authenticate which returns a user if one matches.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
+require 'digest/sha1'
+
 class User < ActiveRecord::Base
+  before_save :digest_password
+  
+  class << self
+    def digest_password(password)
+      Digest::SHA1.hexdigest(password)
+    end
+    
+    def authenticate(username, password)
+      find :first, :conditions => { :username => username, :password => digest_password(password) }
+    end
+  end
+  
+  private
+  
+  def digest_password
+    write_attribute :password, self.class.digest_password(password)
+  end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,19 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 describe "User" do
-  it "should initialize" do
-    
+  before do
+    @user = User.create :username => 'Eloy', :password => 'secret'
+  end
+  
+  it "should digest the password before saving" do
+    @user.password.should.not == 'secret'
+    @user.password.should.not.be.blank
+  end
+  
+  it "should authenticate" do
+    User.authenticate('Eloy', 'secret').should == @user
+    User.authenticate('Eloy', 'notsecret').should.be nil
+    User.authenticate('', 'secret').should.be nil
+    User.authenticate('Eloy', '').should.be nil
   end
 end
