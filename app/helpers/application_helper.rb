bab9491236f32ceacf05983ea3bffe0d458a5124
Eloy Duran <eloy.de.enige@gmail.com>
2008-06-07 18:04:34 +0200

    Added a form on show page which changes the theme by changing the selection in a select control.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  AVAILABLE_THEMES = Dir.glob(File.join(RAILS_ROOT, 'public', 'stylesheets', 'themes', '*.css')).map do |f|
+    File.basename(f)[0..-5]
+  end.map { |t| [t.gsub('_', ' '), t] }
+  
+  def themes_options
+    options_for_select(AVAILABLE_THEMES, @theme)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :pastes
   map.formatted_paste_url '/pastes/:id.:format', :controller => 'pastes', :action => 'show'
-  map.themed_paste_url '/pastes/:id/theme/:theme', :controller => 'pastes', :action => 'show'
 end
